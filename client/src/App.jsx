82ebb7d28a9314d13586438cd6ee7cf537d2561a
Sowdarjya <kolaysowdarjya@gmail.com>
2025-01-20 12:58:35 +0530

    Added initial files

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen w-full flex items-center justify-center">
+      <Button> Click me </Button>
+    </div>
+  );
+};
+
+export default App;
