af340baa8400a7cff4612c4b111c2d46fd53974c
Sowdarjya <kolaysowdarjya@gmail.com>
2025-02-25 20:23:49 +0530

    .

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,9 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full py-6 px-4 text-center transition-colors duration-300 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-center items-center">
-        <p>
-          &copy; {new Date().getFullYear()} My Website. All rights reserved.
-        </p>
+        <p>&copy; {new Date().getFullYear()} InkWell. All rights reserved.</p>
       </div>
     </footer>
   );
