51a3d2d31fc6d053e06986990461f9831ad0b2c3
Sowdarjya <kolaysowdarjya@gmail.com>
2025-03-03 18:27:08 +0530

    Update CORS configuration to allow requests from new frontend URL

--- server/src/index.js
+++ server/src/index.js
@@ -12,9 +12,7 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(
-  cors({ origin: "https://inkwell-m4ly.onrender.com", credentials: true })
-);
+app.use(cors({ origin: "https://ink-well-tau.vercel.app", credentials: true }));
 app.use(express.urlencoded({ limit: "999mb" }));
 app.use(express.json({ limit: "999mb" }));
 app.use(cookieParser());
