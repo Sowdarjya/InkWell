39d15a9ecc639f4bc9f387897e2b0cfe37f9cf81
Sowdarjya <kolaysowdarjya@gmail.com>
2025-03-03 19:51:25 +0530

    Enhance CORS configuration to specify allowed methods for API requests

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,13 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(cors({ origin: "https://ink-well-tau.vercel.app", credentials: true }));
+app.use(
+  cors({
+    origin: "https://ink-well-tau.vercel.app",
+    credentials: true,
+    methods: "GET, POST, PUT, DELETE",
+  })
+);
 app.use(express.urlencoded({ limit: "999mb" }));
 app.use(express.json({ limit: "999mb" }));
 app.use(cookieParser());
